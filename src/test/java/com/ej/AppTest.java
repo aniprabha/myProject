435b292063c7c618d28dd1b3094dc79d7833df4c
aniprabha <50939378+aniprabha@users.noreply.github.com>
2019-07-06 11:47:05 -0400

    myfirst ci test

--- src/test/java/com/ej/AppTest.java
+++ src/test/java/com/ej/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple java app tets
+ * Unit test for anitha
  */
 public class AppTest 
     extends TestCase
